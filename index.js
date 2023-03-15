381fa3fe805489eca266eae3e17679f719941b42
Brad Cornes <hello@bradley.dev>
2023-03-15 13:49:27 +0000

    initial

---
+++ index.js
@@ -0,0 +1,44 @@
+import fs from 'fs'
+import path from 'path'
+import resolve from 'resolve'
+import detective from 'detective-typescript'
+
+function createModule(file) {
+  let source = fs.readFileSync(file, 'utf-8')
+  return { file, requires: detective(source, { mixedImports: true }) }
+}
+
+function* _getModuleDependencies(entryFile) {
+  let mod = createModule(entryFile)
+
+  yield mod
+
+  // Iterate over the modules, even when new
+  // ones are being added
+  for (let dep of mod.requires) {
+    // Only track local modules, not node_modules
+    if (!dep.startsWith('./') && !dep.startsWith('../')) {
+      continue
+    }
+
+    try {
+      let basedir = path.dirname(mod.file)
+      let depPath = resolve.sync(dep, { basedir })
+      yield* _getModuleDependencies(depPath)
+    } catch (_err) {
+      // eslint-disable-next-line no-empty
+    }
+  }
+}
+
+function getModuleDependencies(entryFile) {
+  return Array.from(_getModuleDependencies(entryFile))
+}
+
+function dependencies(path) {
+  return new Set(getModuleDependencies(path).map(({ file }) => file))
+}
+
+// expected: ['/Users/../../one.ts', '/Users/../../two.ts', '/Users/../../three.ts']
+// actual: ['/Users/../../one.ts']
+console.log(dependencies(path.resolve('./one.ts')))
---
+++ two.ts
@@ -0,0 +1,1 @@
+import three from './three'
